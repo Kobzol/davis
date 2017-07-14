704e3f8f1524d8464c01dd0bbd759b23ba4bb8b2
Jakub Beránek <berykubik@gmail.com>
2017-07-14 17:52:08 +0200

    REF: remove generated files

--- src/app/components/app/app.ts
+++ src/app/components/app/app.ts
@@ -31,14 +31,38 @@ export class AppComponent implements AfterViewInit
 `section .data
 hello:
     db 'Hello world!', 10, 0
-
 section .text
-
-asd:
     MOV EAX, hello
-    MOV [EAX], BYTE 97 ; MOV EAX, hello
-    INT 2
-    HLT`
+    INT 2   ; print string EAX
+
+    PUSH 5
+    CALL factorial
+    POP ECX
+    INT 1   ; print EAX
+    HLT
+
+factorial:
+    ENTER
+    
+    CMP [EBP + 8], 1
+    JNE .recurse
+    MOV EAX, 1
+    JMP .end
+    
+.recurse:
+    MOV EAX, [EBP + 8]
+    DEC EAX
+
+    PUSH EAX
+    CALL factorial
+    POP ECX
+    
+    IMUL [EBP + 8]
+    
+.end:
+    LEAVE
+    RET
+`
         ;
     }
 
